1ec5cd15cf3b6854d8eb0a42bf634f0b40d90e52
Parker  Brown <parkerbrown@MacBook-Pro.local>
2021-12-14 23:59:37 -0500

    update for heroku

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const cors = require('cors')
-
+var express = require('express')
 const app = express()
-const port = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
 
-const fn = require('./services/apis')
+const fn = require("./services/apis");
+
+var port = process.env.PORT || 5000;
+
+var host = "0.0.0.0";
+
+var cors = require("cors");
+
+app.use(cors()); // Use this after the variable declaration
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (request, response) => {
 	response.json({ yo: "Its lit fam" });
@@ -13,6 +21,7 @@ app.get("/", (request, response) => {
 app.get('/random', fn.getRandom)
 
 // RUN SERVER -- DEF GONNA BE WRITING THIS EVERYTIME TOO
-app.listen(port, () => {
-	console.log(`App running on port ${port}.`);
+var server = app.listen(port, host, () => {
+	console.log("Server is running on port " + port);
 });
+
